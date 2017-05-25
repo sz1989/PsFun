362371ed73b0700f933a1f8db026f6823dcf4e3d
David Kao <dkao@assuredguaranty.com>
2017-05-25 11:28:56 -0400

    Init

---
+++ New-Archive.ps1
@@ -0,0 +1,3 @@
+Import-Module –Name E:\Tmp\Microsoft.PowerShell.Archive\Microsoft.PowerShell.Archive\Microsoft.PowerShell.Archive.psd1 -Verbose
+
+Compress-Archive -Path E:\Tmp\DocTemplates -CompressionLevel Optimal -DestinationPath E:\Tmp\Draft.Zip
